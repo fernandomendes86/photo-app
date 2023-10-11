dfcc5c2c098662f310d963ed75c79ace5f87fc58
Fernando Mendes <fernando.msantos1@gmail.com>
2023-10-11 00:02:59 -0300

    configuring mailtrap to test sending email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,16 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Mailtrap test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.mailtrap.user_test,
+    password: Rails.application.credentials.mailtrap.password_test,
+    address: 'sandbox.smtp.mailtrap.io',
+    domain: 'sandbox.smtp.mailtrap.io',
+    port: '2525',
+    authentication: :cram_md5
+   }
 end
